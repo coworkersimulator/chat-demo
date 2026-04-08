e663e7b3f3523ee7878afd0101ced44f4920498b
Coworker Simulator <coworkersimulator@gmail.com>
2026-04-07 21:05:36 -0600

    Query dms

--- src/App.tsx
+++ src/App.tsx
@@ -39,17 +39,35 @@ function App() {
 
   useEffect(() => {
     if (!userId) return;
-    // const q = db
-    //   .selectFrom('tag as t')
-    //   .where('t.name', '=', ':dm:')
-    //   .innerJoin('relation as rtn', (join) =>
-    //     join.onRef('rtn.toTagId', '=', 't.id'),
-    //   )
-    //   .innerJoin('relation as rn', (join) =>
-    //     join.onRef('rn.toNoteId', '=', 'rtn.onNoteId'),
-    //   )
-    //   .select(['rtn.onNoteId', 't.name']);
-    // q.execute().then(console.log);
+    const q = db
+      .selectFrom('tag as t')
+      .where('t.name', '=', ':dm:')
+      .innerJoin('rel as rtn', (join) => join.onRef('rtn.asTagId', '=', 't.id'))
+      .innerJoin('rel as rn', (join) =>
+        join.onRef('rn.asNoteId', '=', 'rtn.onNoteId'),
+      )
+      .innerJoin('rel as rp', (join) =>
+        join
+          .onRef('rp.onNoteId', '=', 'rtn.onNoteId')
+          .on('rp.asUserId', 'is not', null),
+      )
+      .innerJoin('rel as rme', (join) =>
+        join
+          .onRef('rme.onNoteId', '=', 'rtn.onNoteId')
+          .on('rme.asUserId', '=', userId),
+      )
+      .innerJoin('note as n', (join) => join.onRef('rn.onNoteId', '=', 'n.id'))
+      .innerJoin('user as u', (join) => join.onRef('u.id', '=', 'rp.asUserId'))
+      .groupBy(['rtn.onNoteId', 'u.id', 'u.username', 'u.name'])
+      .select([
+        'rtn.onNoteId as noteId',
+        'u.id as userId',
+        'u.username',
+        'u.name as userName',
+      ])
+      .select((eb) => eb.fn.max('n.at').as('lastAt'))
+      .orderBy('lastAt', 'desc');
+    q.execute().then(console.log);
   }, [userId]);
 
   async function handleUserChange(id: string) {
